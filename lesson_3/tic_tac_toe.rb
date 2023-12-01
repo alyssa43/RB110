d557ecbe9ed81202cc6c89fc1bd832c498075761
Alyssa Easter <AlyssaStarley@gmail.com>
2023-12-01 14:36:22 -0800

    Completed step 3 of tic tac toe assignment

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -21,6 +21,7 @@ def prompt(msg)
 end
 
 def display_board(brd)
+	system 'clear'
   puts ""
   puts "     |     |     "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -46,7 +47,7 @@ def empty_squares(brd)
 	brd.keys.select {|num| brd[num] == INITIAL_MARKER}
 end
 
-def player_places_piece(brd)
+def player_places_piece!(brd)
 	square = ''
 	loop do
 		prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
@@ -54,14 +55,30 @@ def player_places_piece(brd)
 		break if empty_squares(brd).include?(square)
 		prompt "Sorry, that is not a valid choice."
 	end
-	binding.pry
 	brd[square] = PLAYER_MARKER
-	binding.pry
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+	brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+	empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+	false
 end
 
 board = initialize_board
 display_board(board)
 
-player_places_piece(board)
-puts board.inspect
-display_board(board) 
+loop do 
+	player_places_piece!(board)
+	computer_places_piece!(board)
+	display_board(board)
+	break if someone_won?(board) || board_full?(board)
+end
+
+display_board(board)

a0d15f239eaf6687ba8d4f4f103667cba04ac232
Alyssa Easter <AlyssaStarley@gmail.com>
2023-12-06 17:33:47 -0800

    Finished Tic Tac Toe assignment

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -4,6 +4,9 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
 
+LABELED_BOARD = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5',
+                  6 => '6', 7 => '7', 8 => '8', 9 => '9' }
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -12,18 +15,43 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-# rubocop:disable Layout/LineLength
-def display_scores(scores)
-	prompt "You're an #{PLAYER_MARKER}. Computer is an #{COMPUTER_MARKER}."
-  prompt "First player to score 5 points wins!"
-  prompt "Your Score: #{scores['Player']} - Computer Score: #{scores['Computer']}"
+# rubocop:disable Metrics/MethodLength
+def display_welcome_message(brd)
+  system 'clear'
+  puts "---> Welcome to Tic Tac Toe <--- \n \n"
+  puts <<~WELCOME
+    In this game of Tic Tac Toe you will play against the computer, taking turns
+    putting your marks in empty squares. The squares are numbered as followed:
+    WELCOME
+  display_board(brd)
+  puts <<~RULES
+    The first player to get 3 of their marks in a row (up, down, across, or
+    diagonally) is the winner of that round and recieves 1 point. We will
+    take turns going first.
+
+    If all 9 squares are full and no player has 3 marks in a row, that round
+    ends in a tie and neither player gets a point.
+
+    The first player to reach five points wins the game. Good luck!
+
+  RULES
+  prompt "Push any button to continue when you're ready to play!"
+  gets.chomp
+  system 'clear'
 end
-# rubocop:enable Layout/LineLength
+# rubocop:enable Metrics/MethodLength
+
+def display_scores(scores)
+  system 'clear'
+  puts "---> SCORE <---"
+  puts "You: #{scores['Player']}"
+  puts "Computer: #{scores['Computer']}"
+  puts "You're an #{PLAYER_MARKER}. Computer is an #{COMPUTER_MARKER}."
+  puts "First player to score 5 points wins!"
+end
 
 # rubocop:disable Metrics/AbcSize
-def display_board(brd, scores)
-  system 'clear'
-  display_scores(scores)
+def display_board(brd)
   puts ""
   puts "     |     |     "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -46,38 +74,22 @@ def initialize_board
   new_board
 end
 
-def first_turn(game_count)
-	answer = ''
-	if game_count.even?
-		loop do
-			prompt "You can choose who goes first!"
-			prompt "Choose 'P' for Player (you) -or- 'C' for Computer (me)"
-			answer = gets.chomp.downcase
-			break if answer == 'p' || answer == 'c'
-			prompt "Invalid choice, please try again"
-		end
-	else
-		answer = 'c'
-	end
-	answer
-end
-
 def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def empty_defense_squares(brd)
-	lines_to_defend = WINNING_LINES.select do |line|
-		brd.values_at(*line).count(PLAYER_MARKER) == 2
-	end
-	lines_to_defend.flatten.select { |square| brd[square] == INITIAL_MARKER }
+  lines_to_defend = WINNING_LINES.select do |line|
+    brd.values_at(*line).count(PLAYER_MARKER) == 2
+  end
+  lines_to_defend.flatten.select { |square| brd[square] == INITIAL_MARKER }
 end
 
 def empty_offense_squares(brd)
-	offense_lines = WINNING_LINES.select do |line|
-		brd.values_at(*line).count(COMPUTER_MARKER) == 2
-	end
-	offense_lines.flatten.select { |square| brd[square] == INITIAL_MARKER }
+  offense_lines = WINNING_LINES.select do |line|
+    brd.values_at(*line).count(COMPUTER_MARKER) == 2
+  end
+  offense_lines.flatten.select { |square| brd[square] == INITIAL_MARKER }
 end
 
 def joinor(arr, punc = ', ', wrd = 'or')
@@ -92,6 +104,29 @@ def joinor(arr, punc = ', ', wrd = 'or')
   str
 end
 
+def first_player(game_count)
+  game_count.even? ? 'p' : 'c'
+end
+
+def detect_threat?(brd)
+  threat = false
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2
+      threat = true
+    end
+  end
+  threat
+end
+
+def detect_offensive_move?(brd)
+  offensive_move = false
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(COMPUTER_MARKER) == 2
+      offensive_move = true
+    end
+  end
+  offensive_move
+end
 
 def player_places_piece!(brd)
   square = ''
@@ -99,43 +134,30 @@ def player_places_piece!(brd)
     prompt "Choose a position to place a piece: #{joinor(empty_squares(brd))} "
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that is not a valid choice."
+    puts "Sorry, that is not a valid choice."
   end
   brd[square] = PLAYER_MARKER
 end
 
-def detect_threat?(brd)
-	threat = false
-  WINNING_LINES.each do |line|
-		if brd.values_at(*line).count(PLAYER_MARKER) == 2
-			threat = true
-		end
-	end
-	threat
-end
-
-def detect_offensive_move?(brd)
-	offensive_move = false
-	WINNING_LINES.each do |line|
-		if brd.values_at(*line).count(COMPUTER_MARKER) == 2
-			offensive_move = true
-		end
-	end
-	offensive_move
-end
-
 def computer_places_piece!(brd)
-	square = ''
-	if detect_offensive_move?(brd) && !empty_offense_squares(brd).empty?
-		square = empty_offense_squares(brd).sample
-	elsif detect_threat?(brd) && !empty_defense_squares(brd).empty?
-		square = empty_defense_squares(brd).sample
-	elsif brd[5] == INITIAL_MARKER
-		square = 5
-	else
-	 square = empty_squares(brd).sample
-	end
-	brd[square] = COMPUTER_MARKER
+  square = if detect_offensive_move?(brd) && !empty_offense_squares(brd).empty?
+             empty_offense_squares(brd).sample
+           elsif detect_threat?(brd) && !empty_defense_squares(brd).empty?
+             empty_defense_squares(brd).sample
+           elsif brd[5] == INITIAL_MARKER
+             5
+           else
+             empty_squares(brd).sample
+           end
+  brd[square] = COMPUTER_MARKER
+end
+
+def place_piece!(brd, player)
+  player == 'p' ? player_places_piece!(brd) : computer_places_piece!(brd)
+end
+
+def alternate_player(current_player)
+  current_player == 'p' ? 'c' : 'p'
 end
 
 def board_full?(brd)
@@ -162,58 +184,54 @@ def detect_winner(brd)
 end
 
 def display_winner(winner, board, scores)
-  display_board(board, scores)
+  display_scores(scores)
+  display_board(board)
   if winner == 'Player'
-    prompt "Congratulations, you won!"
+    puts "Congratulations, you won!"
   else
-    prompt "Computer wins"
+    puts "Computer wins"
   end
 end
 
+##################
+# Main Game Loop #
+##################
+
+display_welcome_message(LABELED_BOARD)
+
 loop do # Loop One
   scores = { "Player" => 0, "Computer" => 0 }
-	game_count = 0
+  game_count = 0
 
   loop do # Loop Two
-		system 'clear'
+    system 'clear'
     board = initialize_board
-		player_one = first_turn(game_count)
+    current_player = first_player(game_count)
 
-    if player_one == 'p'
-			loop do 
-				display_board(board, scores)
-        player_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
+    loop do # Loop Three
+      display_scores(scores)
+      display_board(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
+    end # Loop Three
 
-        computer_places_piece!(board)
-        break if someone_won?(board) || board_full?(board)
-			end
-		else
-			loop do
-			  computer_places_piece!(board)
-				break if someone_won?(board) || board_full?(board)
-				display_board(board, scores)
-
-			  player_places_piece!(board)
-			  break if someone_won?(board) || board_full?(board)
-			end
-		end
+    game_count += 1
 
     if someone_won?(board)
       winner = detect_winner(board)
       update_score(scores, winner)
       display_winner(winner, board, scores)
     else
-      display_board(board, scores)
-      prompt "It's a tie!"
+      display_scores(scores)
+      display_board(board)
+      puts "It's a tie!"
     end
-
-		game_count += 1
 
     if scores.value?(5)
       grand_winner = scores.key(5)
       display_winner(grand_winner, board, scores)
-      prompt " - GAME OVER -"
+      puts " ---> GAME OVER <---"
       break
     else
       prompt "Ready for next game? (y or n)"
@@ -227,4 +245,4 @@ loop do # Loop One
   break unless answer.downcase.start_with?('y')
 end # Loop One
 
-prompt "Thanks for playing Tic Tac Toe! Goodbye!"
+puts "Thanks for playing Tic Tac Toe! Goodbye!"

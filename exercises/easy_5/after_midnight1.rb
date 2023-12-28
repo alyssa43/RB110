90955377b580b5bbf851ba5a2dd0ffb1c005bd8a
Alyssa Easter <AlyssaStarley@gmail.com>
2023-12-28 11:53:15 -0800

    Easy 5 exercises completed

--- exercises/easy_5/after_midnight1.rb
+++ exercises/easy_5/after_midnight1.rb
@@ -9,12 +9,16 @@
 
   # - You may not use ruby's Date and Time classes.
 
-	# - input: 
-	# - outputs: 
+	# - input: An Integer, that represents an amount of minutes
+	# - outputs: A String, that represents how many hours and minutes before/after midnight
 
 	# Rules:
 		# Explicit requirements:
-		# - 
+		# - The input Integer can be negative or postivie
+      # - If negative, the time is before midnight
+      # - if positive, the time is after midnight
+    # - The return value must be in 24 hour format (hh:mm)
+    # - Cannot use Ruby's Date and Time classes
 
 		# Implicit requirements:
 		# - 
@@ -33,8 +37,34 @@
 
 # Algorithm: 
 
-	# running_total method
+	# time_of_day method
 	# =============================
 		# - 
 
 # Code:
+
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = 1440
+
+def normalize_minutes_to_0_through_1439(minutes)
+  while minutes < 0
+    minutes += MINUTES_PER_DAY
+  end
+
+  minutes % MINUTES_PER_DAY
+end
+
+def time_of_day(delta_minutes)
+  delta_minutes = normalize_minutes_to_0_through_1439(delta_minutes)
+  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+  format('%02d:%02d', hours, minutes)
+end
+
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
--- exercises/easy_5/letter_swap.rb
+++ exercises/easy_5/letter_swap.rb
@@ -1,37 +1,3 @@
-# PROBLEM:
-
-	# - Given a string of words separated by spaces, write a method that takes this string of words 
-	# - and returns a string in which the first and last letters of every word are swapped.
-
-  # - You may assume that every word contains at least one letter, and that the string will always 
-	# - contain at least one word. You may also assume that each string contains nothing but words and spaces.
-
-	# - input: String of at least one word
-	# - outputs: 
-
-	# Rules:
-		# Explicit requirements:
-		# - 
-
-		# Implicit requirements:
-		# - 
-
-# Examples: 
-  # - swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
-	# - swap('Abcde') == 'ebcdA'
-	# - swap('a') == 'a'
-
-# Data Structure(s): 
-  # - array
-
-# Algorithm: 
-
-	# `swap` method
-	# =============================
-		# - 
-
-# Code:
-
 def swap(str)
   str.split(' ').map do |word|
 		next word if word.length == 1

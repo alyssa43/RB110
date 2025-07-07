f1779ecd550ec8bf0dacaf943d7553f52bcffd26
Alyssa Easter <AlyssaStarley@gmail.com>
2025-07-07 15:33:50 -0700

    Completed more exercises

---
+++ exercises/advanced_1/transpose3x3.rb
@@ -0,0 +1,16 @@
+def transpose(matrix)
+  matrix.map.with_index do |_, index|
+    matrix.map { |row| row[index] }
+  end
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
---
+++ exercises/advanced_1/transposeMxN.rb
@@ -0,0 +1,11 @@
+def transpose(matrix)
+  (0...matrix[0].size).map do |column_index|
+    matrix.map { |row| row[column_index] }
+  end
+end
+
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+  [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
